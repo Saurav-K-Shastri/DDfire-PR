02e6168dab6846e3e79f7a46d38b1f909a785305
shastri.19 <shastri.19@ece-c01187299s.coeit.osu.edu>
2025-09-01 18:27:06 -0400

    updated DDfire-PR

--- scripts/compute_eta_map_ffhq.sh
+++
@@ -1,4 +0,0 @@
-python compute_eta_map.py \
---model_config=configs/ffhq_model_config.yaml \
---diffusion_config=configs/ffhq_diffusion_config.yaml \
---data_config=configs/ffhq_config.yaml
--- scripts/compute_eta_map_imagenet.sh
+++
@@ -1,4 +0,0 @@
-python compute_eta_map.py \
---model_config=configs/imagenet_model_config.yaml \
---diffusion_config=configs/imagenet_diffusion_config.yaml \
---data_config=configs/imagenet_config.yaml
